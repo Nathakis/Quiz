7ce6089b0328200928d570fbed3c85fdc1a17cbe
Artur Ziajkiewicz <arturz.lektor@gmail.com>
2019-01-17 16:49:56 +0100

    okienko 0.0.0
    Nic nie działa

--- QuizOkienkoSFML/main.cpp
+++ QuizOkienkoSFML/main.cpp
@@ -1,33 +1,65 @@
 #include <SFML/Graphics.hpp>
+#include "pytanie.h"
+#include <fstream>
+#include <iostream>
 
+string bufor,window="window";
+int dlugosc_quizu=0;
+int nr_pytania=1;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Quiz SFML",sf::Style::Close);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    setlocale(LC_ALL,"");
+    Pytanie *p;
+    fstream plik;
+    plik.open("quiz.txt",ios::in);
+    while(getline(plik,bufor))
+    {
+        dlugosc_quizu++;
+    }
+    dlugosc_quizu/=6;
+    p = new Pytanie [dlugosc_quizu];
+
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "Quiz SFML",sf::Style::Close);
 
     while (window.isOpen())
     {
         sf::Event event;
+        p[nr_pytania-1].zadaj_pytanie(1000,800);
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)window.close();
             if(event.type == sf::Event::Closed || (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)))window.close();
-            if(event.type == sf::Event::MouseButtonPressed && event.key.code == sf::Mouse::Left)
+            if(event.type==sf::Event::MouseButtonPressed&&event.key.code==sf::Mouse::Left)
             {
-                if(shape.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) == true)
+            if(PrzyciskA->Pytanie::p[nr_pytania].getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) == true)
                 {
-                    shape.setFillColor(sf::Color::White);
+                    odpowiedz='A';
+                    nr_pytania+=1;
+                    p[nr_pytania].sprawdz_odpowiedz();
+                }
+                if(PrzyciskB.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) == true)
+                {
+                    odpowiedz='B';
+                    nr_pytania+=1;
+                    p[nr_pytania-1].sprawdz_odpowiedz();
+                }
+                if(PrzyciskC.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) == true)
+                {
+                    odpowiedz='C';
+                    nr_pytania+=1;
+                    p[nr_pytania-1].sprawdz_odpowiedz();
+                }
+                if(nazwa[4].getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) == true)
+                {
+                    odpowiedz='D';
+                    nr_pytania+=1;
+                    p[nr_pytania-1].sprawdz_odpowiedz();
                 }
             }
-            if(event.type==sf::Event::MouseButtonReleased)
-            {
-                shape.setFillColor(sf::Color::Green);
-            }
-        }
 
+        }
         window.clear();
-        window.draw(shape);
+        p[nr_pytania-1].wyswietl();
         window.display();
     }
 
